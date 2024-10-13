ad9af2d478df22d81c7fe56fac2154e7749e1df8
apirrone <awanegenebayo@gmail.com>
2024-10-13 21:07:00 +0200

    using dof positions instead of dof_obs

--- awd/env/tasks/duckling.py
+++ awd/env/tasks/duckling.py
@@ -853,6 +853,7 @@ def dof_to_obs(pose, dof_obs_size, dof_offsets, dof_axis):
     joint_obs_size = 6
     num_joints = len(dof_offsets) - 1
 
+    dof_obs_size *= joint_obs_size  # Added
     dof_obs_shape = pose.shape[:-1] + (dof_obs_size,)
     dof_obs = torch.zeros(dof_obs_shape, device=pose.device)
     dof_obs_offset = 0
@@ -901,43 +902,58 @@ def compute_duckling_observations(
     projected_gravity,
 ):
     # type: (Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, bool, bool, int, List[int], List[int], Tensor) -> Tensor
-    root_h = root_pos[:, 2:3]
-    heading_rot = torch_utils.calc_heading_quat_inv(root_rot)
 
-    if local_root_obs:
-        root_rot_obs = quat_mul(heading_rot, root_rot)
-    else:
-        root_rot_obs = root_rot
-    root_rot_obs = torch_utils.quat_to_tan_norm(root_rot_obs)
+    print("projected gravity shape", projected_gravity.shape)
+    print("dof po shape", dof_pos.shape)
+    print("dof vel shape", dof_vel.shape)
 
-    if not root_height_obs:
-        root_h_obs = torch.zeros_like(root_h)
-    else:
-        root_h_obs = root_h
-
-    local_root_vel = quat_rotate(heading_rot, root_vel)
-    local_root_ang_vel = quat_rotate(heading_rot, root_ang_vel)
-
-    root_pos_expand = root_pos.unsqueeze(-2)
-    local_key_body_pos = key_body_pos - root_pos_expand
-
-    heading_rot_expand = heading_rot.unsqueeze(-2)
-    heading_rot_expand = heading_rot_expand.repeat((1, local_key_body_pos.shape[1], 1))
-    flat_end_pos = local_key_body_pos.view(
-        local_key_body_pos.shape[0] * local_key_body_pos.shape[1],
-        local_key_body_pos.shape[2],
+    obs = torch.cat(
+        (
+            projected_gravity,
+            dof_pos,
+            dof_vel,
+        ),
+        dim=-1,
     )
-    flat_heading_rot = heading_rot_expand.view(
-        heading_rot_expand.shape[0] * heading_rot_expand.shape[1],
-        heading_rot_expand.shape[2],
-    )
-    local_end_pos = quat_rotate(flat_heading_rot, flat_end_pos)
-    flat_local_key_pos = local_end_pos.view(
-        local_key_body_pos.shape[0],
-        local_key_body_pos.shape[1] * local_key_body_pos.shape[2],
-    )
+    return obs
 
-    dof_obs = dof_to_obs(dof_pos, dof_obs_size, dof_offsets, dof_axis)
+    # root_h = root_pos[:, 2:3]
+    # heading_rot = torch_utils.calc_heading_quat_inv(root_rot)
+
+    # if local_root_obs:
+    #     root_rot_obs = quat_mul(heading_rot, root_rot)
+    # else:
+    #     root_rot_obs = root_rot
+    # root_rot_obs = torch_utils.quat_to_tan_norm(root_rot_obs)
+
+    # if not root_height_obs:
+    #     root_h_obs = torch.zeros_like(root_h)
+    # else:
+    #     root_h_obs = root_h
+
+    # local_root_vel = quat_rotate(heading_rot, root_vel)
+    # local_root_ang_vel = quat_rotate(heading_rot, root_ang_vel)
+
+    # root_pos_expand = root_pos.unsqueeze(-2)
+    # local_key_body_pos = key_body_pos - root_pos_expand
+
+    # heading_rot_expand = heading_rot.unsqueeze(-2)
+    # heading_rot_expand = heading_rot_expand.repeat((1, local_key_body_pos.shape[1], 1))
+    # flat_end_pos = local_key_body_pos.view(
+    #     local_key_body_pos.shape[0] * local_key_body_pos.shape[1],
+    #     local_key_body_pos.shape[2],
+    # )
+    # flat_heading_rot = heading_rot_expand.view(
+    #     heading_rot_expand.shape[0] * heading_rot_expand.shape[1],
+    #     heading_rot_expand.shape[2],
+    # )
+    # local_end_pos = quat_rotate(flat_heading_rot, flat_end_pos)
+    # flat_local_key_pos = local_end_pos.view(
+    #     local_key_body_pos.shape[0],
+    #     local_key_body_pos.shape[1] * local_key_body_pos.shape[2],
+    # )
+
+    # dof_obs = dof_to_obs(dof_pos, dof_obs_size, dof_offsets, dof_axis)
 
     # obs = torch.cat(
     #     (
@@ -952,20 +968,7 @@ def compute_duckling_observations(
     #     dim=-1,
     # )
 
-    obs = torch.cat(
-        (
-            # root_h_obs,
-            # root_rot_obs,
-            # local_root_vel,
-            # local_root_ang_vel,
-            projected_gravity,
-            dof_obs,
-            dof_vel,
-            # flat_local_key_pos,
-        ),
-        dim=-1,
-    )
-    return obs
+    # return obs
 
 
 @torch.jit.script
